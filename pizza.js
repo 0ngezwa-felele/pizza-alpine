50a40b1b53b253d08de3c4ada45f1d90a67125a5
Ongezwa <ongezwaf27@gmail.com>
2022-05-09 16:45:01 +0200

    cart and total price

---
+++ pizza.js
@@ -0,0 +1,6 @@
+// const cartElement = document.querySelector(".smallBtn");
+// const cartElement2 = document.getElementById("#hidden")
+
+// cartElement.addEventListener('click',function){
+//     cartElement2.style.display = 'block'
+// }
